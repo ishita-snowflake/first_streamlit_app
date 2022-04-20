1b371a95f130fc6082dd9b764512ce7c2a35fb61
ISHITA VAID <103979235+ishita-snowflake@users.noreply.github.com>
2022-04-20 20:37:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,5 @@ fruityvice_response=requests.get("https://www.fruityvice.com/api/fruit/" +fruit_
 
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
