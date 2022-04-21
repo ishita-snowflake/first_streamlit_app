7d5cfdff7c73ad9f5bbc4f853754fcbbc604f340
ISHITA VAID <103979235+ishita-snowflake@users.noreply.github.com>
2022-04-21 09:46:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ try:
     fruityvice_response=requests.get("https://www.fruityvice.com/api/fruit/" +fruit_choice)
     fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
- except URLError as 0:
+ except URLError as e:
     streamlit.error()
 
 streamlit.stop()
