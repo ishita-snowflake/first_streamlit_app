06c960429f3fc7c9d2506aaa0f2584906725d400
ISHITA VAID <103979235+ishita-snowflake@users.noreply.github.com>
2022-04-21 09:44:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ try:
     fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
  except URLError as 0:
-    steamlit.error()
+    streamlit.error()
 
 streamlit.stop()
 
